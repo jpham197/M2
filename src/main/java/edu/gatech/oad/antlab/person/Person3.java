8b2465dc9fdb678dad598939d074a8ba38925bdb
Noah Le <noahle@lawn-143-215-60-88.lawn.gatech.edu>
2019-01-23 17:15:57 -0500

    modified the method in Person3 to reverse a String.

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -42,7 +42,11 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+		//Person 3 put your implementation here
+		String back = "";
+		for (int i = (input.length() - 1); i >= 0; i--) {
+				back = back + input.substring(i);
+		}
+	  return back;
 	}
 }
