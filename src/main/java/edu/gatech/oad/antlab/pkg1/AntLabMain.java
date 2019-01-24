034223d67d4895a649b7b00388c2df5afa637d2c
Sook Ji Do <sdo32@gatech.edu>
2019-01-24 17:09:41 -0500

    changed AntLabMain with person6

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -75,7 +75,10 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
                   toPrint += p5.toString("gburdell5");
-		  
+                  //Person6 replace P6 with your name
+                  //and gburde115 with your gt id
+                  Person6 p6 = new Person6("P6");
+                  toPrint += p6.toString("sdo32");
         System.out.println(toPrint);
         
         
